640ad9080792b26a276836e30db30e5ca2b1c762
Nico OR <nohayonr@gmail.com>
2025-11-13 20:16:53 -0600

    init commit

---
+++ app/Main.hs
@@ -0,0 +1,55 @@
+module Main where
+import Numeric.LinearAlgebra.Data
+import Numeric.LinearAlgebra
+
+-- n, m, width, length
+data Dimensions = Dimensions Int Int Int Int
+-- Cost function type signature, TODO needs to accept vector argument
+type Cost = Double -> Double -> Double
+-- Activation function type signature
+type Activation = Double -> Double
+-- Model parameters W_i, b_i
+data Theta = Theta [(Matrix Double, Vector Double)]
+
+data Network = Network Dimensions Theta Cost Activation 
+
+relu :: Activation
+relu x = if x > 0 then x else 0 
+
+euclidean :: Cost 
+euclidean x y = ((x - y)**2)
+
+forward :: Network -> Vector Double -> Vector Double
+forward (Network _ (Theta ts) _ f) x1 = foldl (iter) x1 ts where 
+    iter x (w, b) = cmap f (w #> x + b)
+
+-- computed value -> actual value -> 
+backprop :: Vector Double -> Vector Double -> Network -> Theta
+backprop y_hat y (Network _ (Theta ts) c f) = 
+
+main :: IO ()
+main = do
+  let w1 = (2 >< 2)
+        [ 1.0, -0.5
+        , -0.5, 1.0
+        ]
+      b1 = fromList [0.1, (-0.2)]
+      w2 = (2 >< 2)
+        [ 1.0, 1.0
+        , 0.0, 1.0
+        ]
+      b2 = fromList [0.0, 0.3]
+
+      theta = Theta [(w1, b1), (w2, b2)]
+      dims  = Dimensions 2 2 2 2
+      net   = Network dims theta euclidean relu
+
+      xInput = fromList [1.0, -2.0]
+
+      output = forward net xInput
+
+  putStrLn "Input vector:"
+  print xInput
+
+  putStrLn "Network output:"
+  print output
